8e2f0af48b153e537160fd3eecf91f1cfc037157
Luis Eduardo Sandoval <eduarts1@gmail.com>
2022-09-13 18:56:21 -0500

    First Commit en React

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+ export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "Mi Primer tarea",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "Mi Segunda tarea",
+  },
+  {
+    id: 2,
+    title: "mi tercer tarea",
+    description: "Mi Tercer tarea",
+  },
+];
